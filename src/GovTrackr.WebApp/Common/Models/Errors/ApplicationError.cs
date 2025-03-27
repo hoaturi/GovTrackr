f920926df3437a1e0975109ccd4a1f2d24c4e7e3
Taejun <oink2716@gmail.com>
2025-03-27 18:23:31 +0900

    feat: add ApplicationError class for error handling and include FluentResults package

---
+++ src/GovTrackr.WebApp/Common/Models/Errors/ApplicationError.cs
@@ -0,0 +1,17 @@
+﻿using FluentResults;
+
+namespace GovTrackr.Application.Common.Models.Errors;
+
+public abstract class ApplicationError : Error
+{
+    protected ApplicationError(string message)
+        : base(message)
+    {
+    }
+
+    protected ApplicationError(string code, string message)
+        : base(message)
+    {
+        Metadata.Add("ErrorCode", code);
+    }
+}
