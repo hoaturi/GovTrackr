9b0a0a98cc64ae08fda214862ee065b84e8ebb00
Taejun <oink2716@gmail.com>
2025-03-28 22:21:42 +0900

    feat: implement GetPresidentialAction endpoint

---
+++ src/GovTrackr.WebApp/Features/PresidentialAction/GetPresidentialAction/GetPresidentialActionQuery.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace GovTrackr.Application.Features.PresidentialAction.GetPresidentialAction;
+
+public record GetPresidentialActionQuery(Guid Id) : IRequest<Result<GetPresidentialActionResponse>>;
---
+++ src/GovTrackr.WebApp/Features/PresidentialAction/GetPresidentialAction/GetPresidentialActionResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace GovTrackr.Application.Features.PresidentialAction.GetPresidentialAction;
+
+public record GetPresidentialActionResponse(
+    Guid Id,
+    string Category,
+    string SubCategory,
+    string Title,
+    string Content,
+    DateTime PublishedAt,
+    string Summary,
+    string SourceUrl
+);
