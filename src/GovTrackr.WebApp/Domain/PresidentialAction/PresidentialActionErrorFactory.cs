d7adc63f9e21723cc411284977c20cfcda30ee9d
Taejun <oink2716@gmail.com>
2025-03-28 22:20:57 +0900

    feat: add PresidentialActionErrorFactory for standardized error handling

---
+++ src/GovTrackr.WebApp/Domain/PresidentialAction/PresidentialActionErrorFactory.cs
@@ -0,0 +1,12 @@
+﻿using GovTrackr.Application.Common.Models.Errors;
+
+namespace GovTrackr.Application.Domain.PresidentialAction;
+
+public static class PresidentialActionErrorFactory
+{
+    public static NotFoundError PresidentialActionNotFound(Guid id)
+    {
+        return new NotFoundError($"Presidential Action with ID '{id}' was not found",
+            ErrorCodes.PresidentialActionNotFound);
+    }
+}
