30cbea16bda9043993403dd2c92c3cee5b60af81
Taejun <oink2716@gmail.com>
2025-04-01 04:18:42 +0900

    refactor: simplify ScraperFactory by using service locator pattern

--- src/GovTrackr.ScraperService/Scraping/ScraperFactory.cs
+++ src/GovTrackr.ScraperService/Scraping/ScraperFactory.cs
@@ -1,17 +1,12 @@
 ﻿using GovTrackr.ScraperService.Scraping.Abstractions;
-using GovTrackr.ScraperService.Scraping.Scrapers;
 using Shared.Domain.Common;
 
 namespace GovTrackr.ScraperService.Scraping;
 
-internal class ScraperFactory(ServiceProvider serviceProvider) : IScraperFactory
+internal class ScraperFactory(IServiceProvider serviceProvider) : IScraperFactory
 {
     public IScraper GetScraper(DocumentCategoryType categoryCategory)
     {
-        return categoryCategory switch
-        {
-            DocumentCategoryType.PresidentialAction => serviceProvider.GetRequiredService<PresidentialActionScraper>(),
-            _ => throw new ArgumentException($"No scraper found for document type: {categoryCategory}")
-        };
+        return serviceProvider.GetRequiredKeyedService<IScraper>(categoryCategory);
     }
 }
