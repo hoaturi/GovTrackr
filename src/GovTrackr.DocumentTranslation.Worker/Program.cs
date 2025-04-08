eb2636dc4c03bc0d7674b5c88b3b1014fed20880
Taejun <oink2716@gmail.com>
2025-04-08 20:27:08 +0900

    feat: add translation worker project

---
+++ src/GovTrackr.DocumentTranslation.Worker/Program.cs
@@ -0,0 +1,7 @@
+using GovTrackr.DocumentTranslation.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
