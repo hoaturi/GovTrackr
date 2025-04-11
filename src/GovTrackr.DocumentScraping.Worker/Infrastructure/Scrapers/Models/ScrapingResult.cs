9e64d605857ac67013da3f1e52c5732ed3f9f071
Taejun <oink2716@gmail.com>
2025-04-11 15:25:47 +0900

    chore: remove unused file

--- src/GovTrackr.DocumentScraping.Worker/Infrastructure/Scrapers/Models/ScrapingResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using Shared.Domain.PresidentialAction;
-
-namespace GovTrackr.DocumentScraping.Worker.Infrastructure.Scrapers.Models;
-
-internal record ScrapingResult
-{
-    public List<PresidentialAction> Successful { get; } = [];
-    public List<ScrapingError> Failures { get; } = [];
-}
