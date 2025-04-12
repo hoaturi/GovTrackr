5aa53cbb891e65867318cf8b7d4376274103b832
Taejun <oink2716@gmail.com>
2025-04-13 00:16:11 +0900

    feat: implement GetDigest

---
+++ src/GovTrackr.Api/Features/Subscriptions/Digest/GetDigest/GetDigestQuery.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace GovTrackr.Api.Features.Subscriptions.Digest.GetDigest;
+
+public record GetDigestQuery(Guid Id) : IRequest<Result<GetDigestResponse>>;
---
+++ src/GovTrackr.Api/Features/Subscriptions/Digest/GetDigest/GetDigestResponse.cs
@@ -0,0 +1,10 @@
+﻿using Shared.Domain.Subscription;
+
+namespace GovTrackr.Api.Features.Subscriptions.Digest.GetDigest;
+
+public class GetDigestResponse
+{
+    public DeliveryTime DeliveryTime { get; set; }
+    public DeliveryFrequency DeliveryFrequency { get; set; }
+    public DateTime? LastSentAt { get; set; }
+}
