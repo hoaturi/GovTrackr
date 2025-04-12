c157a17b6f41ad4a5d80bdf41c7ce2e3c87aebc2
Taejun <oink2716@gmail.com>
2025-04-12 17:58:19 +0900

    feat: add conflict error

---
+++ src/GovTrackr.Shared/Common/Errors/ConflictError.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Abstractions.Errors;
+
+namespace Shared.Common.Errors;
+
+public class ConflictError : ApplicationError
+{
+
+    public ConflictError(string message) : base(message)
+    {
+    }
+
+    public ConflictError(string message, string code) : base(message, code)
+    {
+    }
+}
